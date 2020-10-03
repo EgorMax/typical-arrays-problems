ee6b4b6c2ea6c70e4c10a1b1a08a793b80115bdb
EgorMax <65013293+EgorMax@users.noreply.github.com>
2020-10-03 23:49:49 +0400

    fix: change wrong function construction

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-function isArrayValid(array) {
-    if (!array||!array.length) { return false }
-    return true
-}
-
 exports.min = function min (array) {
-  if (array === undefined ||  array === null || array.length === 0) {return 0;}
+  if (array === undefined ||  array === null || array.length === 0) {return 0};
     return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  if (array === undefined ||  array === null || array.length === 0) {return 0;}
+  if (array === undefined ||  array === null || array.length === 0) {return 0};
     return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-    if (isArrayValid) {
-        const reducer = (accumulator, currentValue) => accumulator + currentValue;
-        const preResult = (array.reduce(reducer());
-        return preResult / array.length;
-    } else {return 0};
+  if (array === undefined ||  array === null || array.length === 0) {return 0};
+    return array.reduce((a,b) => (a+b)) / array.length;
 }
