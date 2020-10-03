f3b774bec712af917acf40af97648e74bcb08121
EgorMax <65013293+EgorMax@users.noreply.github.com>
2020-10-03 23:47:31 +0400

    fix: change wrong function construction

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-
 exports.min = function min (array) {
-  if (array === undefined ||  array === null || array.length === 0) {return 0;}
+  if (array === undefined ||  array === null || array.length === 0) {return 0};
     return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  if (array === undefined ||  array === null || array.length === 0) {return 0;}
+  if (array === undefined ||  array === null || array.length === 0) {return 0};
     return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  if (array === undefined ||  array === null || array.length === 0) {return 0;}
-    return array.reduce((a,b) => (a+b))/array.length;
+  if (array === undefined ||  array === null || array.length === 0) {return 0};
+    return array.reduce((a,b) => (a+b)) / array.length;
 }
